5bd025213abc6489f476227d7e37217b687eddb7
thanichandu <124342592+thanichandu@users.noreply.github.com>
2023-02-27 20:15:27 +0530

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,34 +1,16 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import "../App.css";
-// import Child from './Child';
+import Child from './Child';
 
-export default function Search({ searchTerm, setSearchTerm, usersData,sendRemoveList,sendStarList}) {
-
-
-  const [isDeleteMoused, setDeleteMouseOver] = useState(false);
-  const [isStarMoused, setStarMouseOver] = useState(false);
-
-  const handleDeleteMouseOver = () => {
-    setDeleteMouseOver(true)
-  }
-  // const handleStarMouseOver = () => {
-
-  //   (isStarMoused === false) ? (setStarMouseOver(true)) : (setStarMouseOver(false))
-
-  // }
-  const handleMouseDown = () => {
-    setDeleteMouseOver(false);
-  }
-  // const handleOnclickTrash = (event) => {
-  //   // event.target.value = props.id;
-  //   console.log("trash id:" + event.target.value);
-    
-  // }
+export default function Search({ searchTerm, setSearchTerm, usersData, sendRemoveList, sendStarList }) {
 
 
 
-  const [select,setSelectTerm] = useState('');
-  // const [filterData,setFilterData] = useState([]);
+
+
+
+
+  const [select, setSelectTerm] = useState('');
 
 
 
@@ -48,13 +30,14 @@ export default function Search({ searchTerm, setSearchTerm, usersData,sendRemove
 
     setSelectTerm(val)
   }
-  const updatedArray = usersData.filter((eachObj) => (eachObj.first_name.toLowerCase().startsWith(searchTerm.toLowerCase()) || 
-  eachObj.last_name.toLowerCase().startsWith((searchTerm.toLowerCase())) || 
-  eachObj.ip_address.toLowerCase().startsWith((searchTerm.toLowerCase()))))
-  const filterData = select ? updatedArray.filter((eachObj) => (eachObj.gender.toLowerCase() === select)): updatedArray;
+  const updatedArray = usersData.filter((eachObj) => (eachObj.first_name.toLowerCase().startsWith(searchTerm.toLowerCase()) ||
+    eachObj.last_name.toLowerCase().startsWith((searchTerm.toLowerCase())) ||
+    eachObj.ip_address.toLowerCase().startsWith((searchTerm.toLowerCase()))))
+
+  const filterData = select ? updatedArray.filter((eachObj) => (eachObj.gender.toLowerCase() === select)) : updatedArray;
 
 
-  
+
 
 
   return (
@@ -66,43 +49,48 @@ export default function Search({ searchTerm, setSearchTerm, usersData,sendRemove
           <option value="male">Male</option>
           <option value="female">Female</option>
         </select>
-        </div>
-        <table style={{ width: "100%" }}>
+      </div>
+      <table style={{ width: "100%" }}>
+        <tr>
+          <th>id</th>
+          <th>first_name</th>
+          <th>last_name</th>
+          <th>email</th>
+          <th>gender</th>
+          <th>ip_address</th>
+        </tr>
+
+        {
+          filterData.map((eachObj, index) => (
+
             <tr>
-                <th>id</th>
-                <th>first_name</th>
-                <th>last_name</th>
-                <th>email</th>
-                <th>gender</th>
-                <th>ip_address</th>
+              <td>{eachObj.id}</td>
+              <td>{eachObj.first_name}</td>
+              <td>{eachObj.last_name}</td>
+              <td>{eachObj.email}</td>
+              <td>{eachObj.gender}</td>
+              <td>{eachObj.ip_address}</td>
+              {/* <td className="btn"><i className="fa fa-trash"
+                            data-id={eachObj.id}  onClick={() => sendRemoveList(eachObj.id,index)}
+                            ></i></td> */}
+              <Child rowId={eachObj.id} index={index} sendRemoveList={sendRemoveList} sendStarList={sendStarList} />
+
+              {/* <td className="btn"><i className="fa fa-star"
+                onClick={() => {
+                  sendStarList(eachObj.id, index, singleStarredRecord,
+                    (singleStarredRecord === false) ? (setSingleStarredRecord(true)) : (setSingleStarredRecord(false)))
+                }} ></i></td> */}
             </tr>
-
-            {
-                filterData.map((eachObj,index) => (
-
-                    <tr>
-                        <td>{eachObj.id}</td>
-                        <td>{eachObj.first_name}</td>
-                        <td>{eachObj.last_name}</td>
-                        <td>{eachObj.email}</td>
-                        <td>{eachObj.gender}</td>
-                        <td>{eachObj.ip_address}</td>
-                        <td className="btn"><i className="fa fa-trash"
-                            data-id={eachObj.id} onClick={() => sendRemoveList(eachObj.id,index)}
-                            onMouseOver={handleDeleteMouseOver}
-                            onMouseOut={handleMouseDown}
-                            style={{ color: (isDeleteMoused) ? "red" : "" }}></i></td>
-
-                        <td className="btn"><i className="fa fa-star"
-                         onClick={() => sendStarList(eachObj.id,index)}
-                          style={{ color: (isStarMoused) ? "yellow" : "" }}></i></td>
-                    </tr>
-
-                ))
-            }
-        </table>
-
+          ))
+        }
+      </table>
 
     </>
   )
 }
+
+
+
+
+
+
